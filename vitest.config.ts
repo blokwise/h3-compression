3ef2d3a4b14387b6dde57088827cba42c47eebef
Julian Derungs <julian.derungs@deepbase.ch>
2025-10-31 23:47:32 +0100

    test: update vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    setupFiles: ['./test/_setup'],
     coverage: {
       reporter: ['text', 'clover', 'json'],
+      provider: 'v8',
     },
   },
 })
